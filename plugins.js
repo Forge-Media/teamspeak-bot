210ee28de8b46e842ae8fef0e85bbaa26f06beed
Jeremy Paton <jeremy@jesararts.net>
2018-11-06 02:41:53 +0000

    Cognitive Complexity refactor

--- plugins.js
+++ plugins.js
@@ -9,11 +9,10 @@ function fetchPlugin(name) {
 
 let plugins = [];
 
-exports.init = function(config_plugins) {
+exports.init = config_plugins => {
 	if (typeof config_plugins == "object") {
 		for (name in config_plugins) {
-			let plugin_state = config_plugins[name];
-			if (plugin_state === true) {
+			if (config_plugins[name]) {
 				plugins.push(fetchPlugin(name));
 			}
 		}
@@ -21,37 +20,37 @@ exports.init = function(config_plugins) {
 	return exports;
 };
 
-exports.startPlugins = function(helpers) {
-	plugins.forEach(function(item) {
+exports.startPlugins = helpers => {
+	plugins.forEach(item => {
 		if (typeof item.run == "function") {
 			item.run(helpers);
 		}
 	});
 };
 
-exports.onMessage = function(msg, jarvis) {
-	plugins.forEach(function(item) {
+exports.onMessage = (msg, jarvis) => {
+	plugins.forEach(item => {
 		if (typeof item.onMessage == "function") {
 			item.onMessage(msg, jarvis);
 		}
 	});
 };
 
-exports.getHelpMessage = function() {
+exports.getHelpMessage = () => {
 	let response = "[b]Jarvis Assistant Bot - Commands:[/b] \n";
 
 	[].concat
 		.apply(
 			[],
 			plugins
-				.filter(function(item) {
+				.filter(item => {
 					return typeof item.help != "undefined";
 				})
-				.map(function(item) {
+				.map(item => {
 					return item.help;
 				})
 		)
-		.map(function(item) {
+		.map(item => {
 			response += "[b]" + item[0] + "[/b]  -  " + item[1] + "\n";
 		});
 
