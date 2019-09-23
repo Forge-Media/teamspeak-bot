7e0b1eb6c94650c238aa59c0cd2cc4a7c59676b8
Jeremy Paton <jeremy@jesararts.net>
2019-09-23 02:54:45 +0100

    v0.9.2 updated example config

--- config/config.example.js
+++ config/config.example.js
@@ -16,11 +16,11 @@ module.exports = {
 		purgeVerified: true, // Allows the '!purgeVerified' command. Requires Steam-TS Intergration to be enabled (https://github.com/Forge-Media/steam-ts)
 		joinMe: true, // Allows the '!joinMe' command
 		help: true, // Allows the '!help' command (recommended)
-		cid: true // Allows the '!cid' command (recommended)
+		cid: true, // Allows the '!cid' command (recommended)
+		registerCSGO: true // Allows the '!registerCSGO <Steam64id>|!deregisterCSGO|!statusCSGO' commands
 	},
-	// NOTE: Can be left empty to disable these integration
 	integrations: {
-		slackBot: {
+		slackHelper: {
 			logsChannelID: "", // The channel-ID of a specific Slack channel for log messages to be sent to
 			config: {
 				token: "", // Your bot's Slack-app token, get here: https://api.slack.com/apps
@@ -32,6 +32,13 @@ module.exports = {
 				owners: [], // Server group IDs which can use this integration (used for !purgeVerified)
 				group_id: 40, // Server group ID, used to compare against SteamTS's verified database
 				database_file: "contrib/data/verified.json" // Directory of your SteamTS's verified database (consider using a Symbolic-Link)
+			}
+		},
+		steamHelper: {
+			config: {
+				account_name: "", // Steam bot account user name
+				password: "", // Steam bot account password
+				auth_code: "" // Steam Guard Code. Leave empty on first launch to receive email, enter code and re-launch bot, THEN REMOVE CODE!!
 			}
 		}
 	},
